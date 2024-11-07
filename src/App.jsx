2de943dba78b0cba78ff3870969be261bf5aac51
TristanGG <azuolympic@gmail.com>
2024-11-07 23:09:08 +0700

    halaman_app

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Gacorr kanggg</h1>
+      <h1>Gacorr tehhh</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
